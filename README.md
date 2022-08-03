29bc3ee2466b55eb2e40607303cf08669c86c515
volkan <volkandogru11@gmail.com>
2022-08-03 18:14:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![bildiirm](bildirim.png);
---
+++ script.js
@@ -0,0 +1,17 @@
+var notify = document.querySelector(".notify");
+var btn = document.querySelector(".btn");
+btn.addEventListener("click", active);
+
+function active() {
+  var add = Number(notify.getAttribute("data-count") || 0);
+  notify.setAttribute("data-count", add + 1);
+  var audio = new Audio("ses.mp3");
+  audio.play();
+  
+
+  if (add === 0) {
+    notify.classList.add("add-numb");
+  }
+
+  
+}
